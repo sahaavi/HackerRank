f40edd12dc3eb366d1055406d623f50d60271762
Avishek Saha <avisheksaha123@gmail.com>
2020-04-09 23:13:52 +0600

    Solution in C#

---
+++ Algorithms/Warmup/Birthday Cake Candles/Solution.cs
@@ -0,0 +1,30 @@
+using System;
+using static System.Console;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        //No need to capture the size of array. I use array's length property instead.
+        ReadLine();
+        var height_temp = ReadLine().Split(' ');
+        var height = Array.ConvertAll(height_temp, int.Parse);
+        var maxValue = height[0];
+        var maxValueOccurence = 1;
+
+        for (int i = 1; i < height.Length; i++)
+        {
+            if (height[i] == maxValue)
+            {
+                maxValueOccurence++;
+                continue;
+            }
+            if (height[i] > maxValue)
+            {
+                maxValue = height[i];
+                maxValueOccurence = 1;
+            }
+        }
+        WriteLine(maxValueOccurence);
+    }
+}

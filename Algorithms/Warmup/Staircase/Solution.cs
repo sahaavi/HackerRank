937d727144e282bd22720ecea929f7d46f3b34ca
Avishek Saha <avisheksaha123@gmail.com>
2020-04-17 23:39:24 +0600

    Solution in C#

--- Algorithms/Warmup/Staircase/Solution.cs
+++ Algorithms/Warmup/Staircase/Solution.cs
@@ -1,20 +1,6 @@
-using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
 using System;
 
 class Solution {
-
-    // Complete the staircase function below.
     static void staircase(int n) {
         for (int i = 1; i <= n; i++)
         {

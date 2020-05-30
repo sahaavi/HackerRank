3ebb570770dbb239f01a8c558b64b36cfe59c999
Ziad Mohib <52622064+zdmohib@users.noreply.github.com>
2020-05-30 17:24:02 +0600

    Solution in C++

---
+++ Algorithms/Warmup/Simple Array Sum/Solution.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+
+int main()
+{
+
+
+    long long int ar_count,sum=0,i;
+
+
+    cin >> ar_count;
+
+
+    long array[ar_count];
+
+    for(i=0;i<ar_count;i++)
+    {
+
+        cin>>array[i];
+
+    }
+
+    for(i=0 ; i<ar_count ; i++ )
+    {
+        sum=array[i]+sum;
+    }
+
+
+    cout<<sum;
+
+    return 0;
+}
